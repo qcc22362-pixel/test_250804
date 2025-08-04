50ae3dc73672f4d886feb987f60b621b145a6b58
qcc22362-pixel <qcc22362@gmail.com>
2025-08-04 16:34:52 +0900

    2 files

---
+++ js/cube.js
@@ -0,0 +1,18 @@
+//아이디가 circle인 요소를 찾아서 저장
+const circle  = document.querySelector("#circle");
+//태그명이 article인 요소를 찾아서 저장
+const article  = document.querySelectorAll("article");
+
+for(let el of article){
+    el.addEventListener("mouseenter",e=>{
+        //article 에 마우스를 올리면 부모인 #circle의 animationPlaystate 값을paused 변경
+        circle.style.animationPlayState="paused";
+    })
+
+
+    el.addEventListener("mouseleave",e=>{
+        ///article 에 마우스를 올리면 부모인 #circle의 animationPlaystate 값을 runing 변경
+        circle.style.animationPlayState="running";
+    })
+
+}
